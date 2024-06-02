044dc83973c2328119601545e7fbcfba1a3e4256
vikashkumaraaryan <161560975+vikashkumaraaryan@users.noreply.github.com>
2024-06-02 11:22:30 +0530

    stote in main kotlin
    main kotlin store

--- app/src/main/java/com/example/email_authentication/MainActivity.kt
+++ app/src/main/java/com/example/email_authentication/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.email_authentication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+     lateinit var email:EditText
+     lateinit var password:EditText
+     lateinit var confirmPassword:EditText
+     lateinit var submit:Button
+
+    @SuppressLint("MissingInflatedId" , "CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left , systemBars.top , systemBars.right , systemBars.bottom)
             insets
         }
+
+        email=findViewById(R.id.email)
+        password=findViewById(R.id.password)
+        confirmPassword=findViewById(R.id.confirm_password)
+        submit=findViewById(R.id.confirm_password)
+
+
     }
 }
